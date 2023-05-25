e036b43ffdc3705c04bfefa16501268fecc780ba
vladimir <sokoltsovvs@gmail.com>
2023-05-25 22:52:21 +0600

    add master

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        String message = "Hello world!";
+        String message = "Hello world!!!";
         System.out.println(message);
     }
 }
